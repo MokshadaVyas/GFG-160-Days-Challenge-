9108ec17071575c1f5666965abda5a47369fe2ac
Sahil <118194056+Sahilll94@users.noreply.github.com>
2024-11-24 13:31:50 +0530

    Create Kadane's-Algorithm.cpp

---
+++ Kadane's-Algorithm.cpp
@@ -0,0 +1,56 @@
+// Given an integer array arr[]. You need to find the maximum sum of a subarray.
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+        int sum = 0;
+        int ans = INT_MIN;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            sum += arr[i];
+            
+            ans = max(ans,sum);
+            
+            if(sum<0)
+            {
+                sum = 0;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
+// } Driver Code Ends
