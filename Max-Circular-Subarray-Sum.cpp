59cb85e7f62479c9a285ca333e17a9c2565bc23f
Sahil <118194056+Sahilll94@users.noreply.github.com>
2024-11-26 22:14:38 +0530

    Update Max-Circular-Subarray-Sum.cpp

--- Max-Circular-Subarray-Sum.cpp
+++ Max-Circular-Subarray-Sum.cpp
@@ -10,6 +10,32 @@ class Solution {
   public:
     // arr: input array
     // Function to find maximum circular subarray sum.
+
+    int method2(vector<int>)
+    {
+        // your code here
+        int n = arr.size();
+      
+        int max_ans = INT_MIN, min_ans = INT_MAX, sum = 0, max_sum = 0, min_sum = 0;
+      
+        for(int i = 0; i < n; i++)
+        {
+            max_sum = max(max_sum, 0) + arr[i];
+            max_ans = max(max_ans, max_sum);
+            min_sum = min(min_sum, 0) + arr[i];
+            min_ans = min(min_ans, min_sum);
+            sum += arr[i];
+        }
+      
+        if(min_ans == sum)
+        {
+            return max_ans;
+        }
+      
+        return max(max_ans, sum - min_ans);
+        
+    }
+
     
     int subArray(vector<int> &arr)
     {
