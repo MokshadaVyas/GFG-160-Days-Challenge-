aa700ddc4de9c2f0ab66cdf54cf6007ab8cc8c6a
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-11-29 16:14:47 +0530

    Implement-Atoi.cpp

---
+++ Implement-Atoi.cpp
@@ -0,0 +1,44 @@
+// Given a string s, the objective is to convert it into integer format without utilizing any built-in functions. Refer the below steps to know about atoi() function.
+// Cases for atoi() conversion:
+//1. Skip any leading whitespaces.
+//2. Check for a sign (‘+’ or ‘-‘), default to positive if no sign is present.
+//3. Read the integer by ignoring leading zeros until a non-digit character is encountered or end of the string is reached. If no digits are present, return 0.
+//4. If the integer is greater than 231 – 1, then return 231 – 1 and if the integer is smaller than -231, then return -231.
+
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int myAtoi(char* s) {
+    int sign = 1, res = 0, idx = 0;
+
+    // Ignore leading whitespaces
+    while (s[idx] == ' ') {
+        idx++;
+    }
+
+    // Store the sign of number
+    if (s[idx] == '-' || s[idx] == '+') {
+          if(s[idx++] == '-')
+          sign = -1;
+    }
+
+    // Construct the number digit by digit
+    while (s[idx] >= '0' && s[idx] <= '9') {
+        
+        // handling overflow/underflow test case
+        if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[idx] - '0' > 7)) {
+            return sign == 1 ? INT_MAX : INT_MIN;
+        }
+      
+        // Append current digit to the result
+        res = 10 * res + (s[idx++] - '0');
+    }
+    return res * sign;
+}
+
+int main() {
+    char s[] = "  -0012g4";
+    cout << myAtoi(s);
+    return 0;
+}
